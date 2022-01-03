38b272ca903f272b468b99fa816e43c71dd35265
misraved <ved@turbot.com>
2022-01-03 18:06:13 +0530

    Add queries for aws lambda

--- query/lambda/lambda_function_dead_letter_queue_configured.sql
+++ query/lambda/lambda_function_dead_letter_queue_configured.sql
@@ -5,9 +5,9 @@ select
     else 'ok'
   end as status,
   name || case
-    when (arguments -> 'dead_letter_config') is null then ' not configured with dead-letter queue.'
-    else  ' configured with dead-letter queue.'
-  end as reason,
+    when (arguments -> 'dead_letter_config') is null then ' not configured with dead-letter queue'
+    else  ' configured with dead-letter queue'
+  end || '.' as reason,
   path
 from
   terraform_resource
