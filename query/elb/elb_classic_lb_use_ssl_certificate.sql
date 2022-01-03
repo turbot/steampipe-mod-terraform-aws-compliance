7d560ea1d05d21a3c46ed9d2b6cbd7bec4336baf
misraved <ved@turbot.com>
2022-01-03 21:59:41 +0530

    Add queries for elb resources

---
+++ query/elb/elb_classic_lb_use_ssl_certificate.sql
@@ -0,0 +1,19 @@
+select
+  type || ' ' || name as resource,
+  case
+    when (arguments -> 'listener') is null then 'skip'
+    when (arguments -> 'listener' ->> 'lb_protocol') ilike 'SSL' and (arguments -> 'listener' -> 'ssl_certificate_id') is null then 'alarm'
+    when (arguments -> 'listener' ->> 'lb_protocol') ilike 'SSL' and (arguments -> 'listener' ->> 'ssl_certificate_id') like 'arn:aws:acm%' then 'ok'
+    else 'alarm'
+  end status,
+  name || case
+    when (arguments -> 'listener') is null then ' has no listener'
+    when (arguments -> 'listener' ->> 'lb_protocol') ilike 'SSL' and (arguments -> 'listener' -> 'ssl_certificate_id') is null then ' does not use certificate provided by ACM'
+    when (arguments -> 'listener' ->> 'lb_protocol') ilike 'SSL' and (arguments -> 'listener' ->> 'ssl_certificate_id') like 'arn:aws:acm%' then ' uses certificates provided by ACM'
+    else ' does not use certificate provided by ACM'
+    end || '.' reason,
+    path
+from
+  terraform_resource
+where
+  type = 'aws_elb';
