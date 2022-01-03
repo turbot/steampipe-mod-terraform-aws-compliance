c8d188391e0445ab42cd837cd77adca4ab3c072e
misraved <ved@turbot.com>
2022-01-03 13:45:06 +0530

    Add the remaining redshift queries

--- query/redshift/redshift_cluster_encryption_logging_enabled.sql
+++ query/redshift/redshift_cluster_encryption_logging_enabled.sql
@@ -1,11 +1,13 @@
 select
   type || ' ' || name as resource,
   case
+    when (arguments -> 'encrypted') is null then 'alarm'
     when (arguments -> 'logging') is null then 'alarm'
     when (arguments -> 'logging' ->> 'enabled')::bool then 'ok'
     else 'alarm'
   end status,
   name || case
+    when (arguments -> 'encrypted') is null then ' not encrypted'
     when (arguments -> 'logging') is null then ' audit logging disabled'
     when (arguments -> 'logging' ->> 'enabled')::bool then ' audit logging enabled'
     else ' audit logging disabled'
