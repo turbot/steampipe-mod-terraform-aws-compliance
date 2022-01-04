00059454590e61afd2715d36bb03a04613a14ce4
Khushboo <khushboo@turbot.com>
2022-01-04 11:02:42 +0530

    added new queries

---
+++ query/sagemaker/sagemaker_notebook_instance_encryption_at_rest_enabled.sql
@@ -0,0 +1,15 @@
+select
+  type || ' ' || name as resource,
+case
+  when (arguments -> 'kms_key_id') is null then 'alarm'
+  else 'ok'
+end status,
+name || case
+  when (arguments -> 'kms_key_id') is null then ' encryption at rest not enabled'
+  else ' encryption at rest enabled'
+ end || '.' reason,
+  path
+from
+  terraform_resource
+where
+  type = 'aws_sagemaker_notebook_instance';
