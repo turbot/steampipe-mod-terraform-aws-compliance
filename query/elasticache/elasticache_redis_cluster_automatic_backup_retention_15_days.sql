c02c216065a16d605059f2bb02ff3f2d39133b6e
Khushboo <khushboo@turbot.com>
2022-01-05 20:23:04 +0530

    added new queries

---
+++ query/elasticache/elasticache_redis_cluster_automatic_backup_retention_15_days.sql
@@ -0,0 +1,16 @@
+select
+  type || ' ' || name as resource,
+  case
+    when (arguments -> 'snapshot_retention_limit')::int < 15 then 'alarm'
+    else 'ok'
+  end status,
+  name || case
+     when (arguments -> 'snapshot_retention_limit')::int is null then ' automatic backups not enabled'
+    when (arguments -> 'snapshot_retention_limit')::int < 15 then ' automatic backup retention period is less than 15 days'
+    else ' automatic backup retention period is more than 15 days'
+  end || '.' reason,
+  path
+from
+  terraform_resource
+where
+  type = 'aws_elasticache_replication_group';
