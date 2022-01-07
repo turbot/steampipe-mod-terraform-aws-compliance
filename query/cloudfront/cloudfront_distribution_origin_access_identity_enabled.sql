907d241d0c927522b90f5a31658cdc96ed456110
Khushboo <khushboo@turbot.com>
2022-01-07 13:44:26 +0530

    added one query

---
+++ query/cloudfront/cloudfront_distribution_origin_access_identity_enabled.sql
@@ -0,0 +1,34 @@
+with cloudfront_distribution as (
+  select
+    *
+  from
+    terraform_resource
+  where
+    type = 'aws_cloudfront_distribution'
+), origins as (
+   select
+      count(*),
+      name
+    from
+      cloudfront_distribution,
+      jsonb_array_elements(arguments -> 'origin') as o
+    where
+      (o  ->> 'domain_name' ) like '%aws_s3_bucket%' and
+      (o -> 's3_origin_config' ->> 'origin_access_identity') = ''
+    group by name
+)
+select
+  -- Required Columns
+  type || ' ' || a.name as resource,
+  case
+    when b.name is not null then 'alarm'
+    else 'ok'
+  end as status,
+  case
+    when b.name is not null then ' origin access identity not configured'
+    else ' origin access identity configured'
+  end || '.' reason,
+  path
+from
+  cloudfront_distribution as a
+  left join origins as b on a.name = b.name
