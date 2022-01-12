d98fed203f3ccd733906711401b9c5f114533d34
rajmohanty17 <raj@turbot.com>
2022-01-12 20:07:02 +0530

    Query change

--- query/vpc/vpc_subnet_auto_assign_public_ip_disabled.sql
+++ query/vpc/vpc_subnet_auto_assign_public_ip_disabled.sql
@@ -2,12 +2,12 @@ select
   type || ' ' || name as resource,
   case
     when (arguments -> 'map_public_ip_on_launch') is null then 'ok'
-    when (arguments -> 'map_public_ip_on_launch')::bool then 'alarm'
+    when (arguments ->> 'map_public_ip_on_launch')::bool then 'alarm'
     else 'ok'
   end status,
   name || case
     when (arguments -> 'map_public_ip_on_launch') is null then ' ''map_public_ip_on_launch'' disabled'
-    when (arguments -> 'map_public_ip_on_launch')::bool then ' ''map_public_ip_on_launch'' enabled'
+    when (arguments ->> 'map_public_ip_on_launch')::bool then ' ''map_public_ip_on_launch'' enabled'
     else ' ''map_public_ip_on_launch'' disabled'
   end || '.' reason,
   path
